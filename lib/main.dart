8e5bf4d3f8785c2e59ac3ea1420c809e9188eae0
AdrielVBomfim <adrielvb@dcomp.ufs.br>
2019-10-02 18:43:11 -0300

    Reorganização dos arquivos de código e implementação do layout de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:comunicalab_frontend/pages/login.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,52 +7,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ComunicaLab',
       theme: ThemeData(
           primaryColor: Color(0xFFF8F8FF),
           fontFamily: 'Roboto',
-          textTheme: TextTheme(
-              headline: TextStyle(color: Color(0xFF000080)),
-              body1: TextStyle(color: Color(0xFF000080)),
-              title: TextStyle(color: Color(0xFF000080)))),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var _opacity = 0.0;
-
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(Duration(milliseconds: 400), () => setState(() {
-        _opacity = 1.0;
-        Future.delayed(Duration(seconds: 3), () => setState(() {
-          _opacity = 0.0;
-        }));
-      }));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FractionallySizedBox(
-          heightFactor: 0.18,
-          child: AnimatedOpacity(
-              opacity: _opacity,
-              duration: Duration(milliseconds: 500),
-              child: Image.asset('assets/images/logo_dcomp.jpg')),
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                  borderSide: BorderSide(color: Color(0xFF000080), width: 2.0)),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                  borderSide:
+                      BorderSide(color: Color(0xFF000080), width: 2.0))),
+          primaryTextTheme: Theme.of(context)
+              .primaryTextTheme
+              .apply(bodyColor: Color(0xFF000080)),
+          accentTextTheme: Theme.of(context)
+              .primaryTextTheme
+              .apply(bodyColor: Color(0xFF4682B4))
+      ),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/pages/login.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var _opacity = 0.0;
+  var _ifLogin = false;
+
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await Future.delayed(Duration(milliseconds: 400), (){
+        setState(() => _opacity = 1.0);
+      });
+
+      await Future.delayed(Duration(seconds: 3), (){
+        setState(() => _opacity = 0.0);
+      });
+
+      await Future.delayed(Duration(milliseconds: 600), (){
+        setState(() => _ifLogin = true);
+      });
+
+      await Future.delayed(Duration(milliseconds: 200), (){
+        setState(() => _opacity = 1.0);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: AnimatedOpacity(
+              opacity: _opacity,
+              duration: Duration(milliseconds: 500),
+              child: Center(
+                  child: _ifLogin
+                      ? FractionallySizedBox(
+                      widthFactor: 0.8,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: "Login",
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 15.0)),
+                          TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Senha",
+                            ),
+                            keyboardType: TextInputType.visiblePassword,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          Padding(padding: EdgeInsets.only(top: 25.0)),
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0)),
+                            color: Color(0xFF000080),
+                            textColor: Color(0xFFFFFFFF),
+                            onPressed: () {},
+                            child: new Text(
+                              "Entrar",
+                            ),
+                          )
+                        ],
+                      ))
+                      : FractionallySizedBox(
+                    heightFactor: 0.2,
+                    child: Image.asset('assets/images/logo_dcomp.jpg'),
+                  )))),
+    );
+  }
+}
