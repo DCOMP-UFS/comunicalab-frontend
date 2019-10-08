f6979202811baa55f4e5b033e3fbd45658a63f5c
kelveng.magalhaes <kelveng.magalhaes@tupiconsultoria.com.br>
2019-10-08 19:42:22 -0300

    implementação inicial do bloc de equipamentos

---
+++ lib/app/src/equipamentos/EquipamentoWidget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EquipamentoWidget extends StatefulWidget {
+  @override
+  
+  _EquipamentoWidgetState createState() => _EquipamentoWidgetState();
+}
+
+
+class _EquipamentoWidgetState extends State<EquipamentoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
